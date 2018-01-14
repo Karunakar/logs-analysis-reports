bd7aff5a7098f0e2d4f2f35cf338f5631c2a904b
vagrant <vagrant@vagrant.vm>
2018-01-14 18:59:01 +0000

    added log analysis outputt

--- README.md
+++ README.md
@@ -2,8 +2,42 @@
 Source code for logs-analysis project.
 
 # Instalation
-Download this and Open 
+
+## The virtual machine
+	This project makes use of the same Linux-based virtual machine (VM) as the preceding lessons.
+
+	If you skipped those lessons and came right to this project, that's OK! However, you will need to go back to these instructions to install the virtual machine. This will give you the PostgreSQL database and support software needed for this project. If you have used an older version of this VM, you may need to install it into a new directory.
+
+
+	Successfully logged into the virtual machine.
+
+
+#### Launching the Vigrant Virtual Machine
+  1. Launch the Vagrant VM inside Vagrant 
+  
+  ```
+    $ vagrant up
+  ```
+  2. Then Log into this using command:
+  
+  ```
+    $ vagrant ssh
+  ```
+
+#### Downlaod the data
+  1. Launch the Vagrant VM inside Vagrant 
+  
+  ```
+    $ vagrant up
+  ```
+  2. Then Log into this using command:
+  
+  ```
+    $ vagrant ssh
+  ```
+  ```
 
 ### License
 This is opensource.
 Email me if you  need any help revurikarna@gmail.com
+
--- log_analysis.py
+++ log_analysis.py
@@ -4,7 +4,6 @@ import psycopg2
 
 DBNAME = "news"
 
-
 def connect():
     return psycopg2.connect("dbname=news")
 
@@ -16,37 +15,38 @@ def get_query_results(query):
     results = c.fetchall()
     return results
 
-
+	
 def display_query_results(query_results):
     for i in range(len(query_results)):
         title = query_results[i][0]
         views = query_results[i][1]
-        print("%s == %d" % (title, views))
+        print("    %s == %d" % (title, views))
 
 
 def display_error_results(query_results):
-    
     for i in range(len(query_results)):
         print(
-            "%s == %s  == %s" %
-            (query_results[i][0], query_results[i][1]), "Errors"	)
+            "    %s == %s  == %s "%
+            (query_results[i][0], query_results[i][1], "Errors"))
 
-
-print("What are the most popular three articles of all time?")
+print("*******************************************************")
+print("What r d most popular 3 articles of all time?")
 
 query_for_first_3_records = "SELECT title, count(title) \
     AS views FROM articles, log \
     WHERE log.path LIKE concat('%',articles.slug) \
-    GROUP BY articles.title, articles.author ORDER BY views DESC "
+    GROUP BY articles.title, articles.author ORDER BY views DESC  limit 3"
 
 
 get_first_3_articles = get_query_results(query_for_first_3_records)
 
 display_query_results(get_first_3_articles)
 
+print("*******************************************************")
+print("\n")
 
-print("What are the most popular three articles of all time?")
 
+print("What are the most popular authors?")
 
 query_for_most_popular_authors = "select authors.name, count(*) as views from articles inner \
 join authors on articles.author = authors.id inner join log \
@@ -55,8 +55,13 @@ join authors on articles.author = authors.id inner join log \
 
 
 get_famous_authors = get_query_results(query_for_most_popular_authors)
+get_famous_authors = display_error_results(get_famous_authors)
 
-print("On which days did more than 1% of requests lead to errors?")
+print("*******************************************************")
+print("\n")
+
+
+print("Dates which r more than 1% lead to errors?")
 query_to_get_date_errors_morethan_1 = "select day, perc from ( \
     select day, round((sum(requests)/(select count(*) from log where \
     substring(cast(log.time as text), 0, 11) = day) * 100), 2) as \
@@ -64,8 +69,8 @@ query_to_get_date_errors_morethan_1 = "select day, perc from ( \
     count(*) as requests from log where status like '%404%' group by day) \
     as log_percentage group by day order by perc desc) as final_query \
     where perc >= 1  "
-	
-get_famous_authors = get_query_results(query_to_get_date_errors_morethan_1)
-	
-get_famous_authors = display_error_results(get_famous_authors)
 
+get_famous_authors = get_query_results(query_to_get_date_errors_morethan_1)
+
+get_famous_authors = display_error_results(get_famous_authors)
+print("*******************************************************")
