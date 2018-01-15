910e01ccafffad8748a4a236c802bdadd20ca775
Karuna <kr8783@att.com>
2018-01-15 12:20:30 +0530

    minor fixes

--- log_analysis.py
+++ log_analysis.py
@@ -8,7 +8,7 @@ def connect():
     return psycopg2.connect("dbname=news")
 
 
-def get_query_results(query):
+def pull_query_results_from_db(query):
     db = connect()
     c = db.cursor()
     c.execute(query)
@@ -18,27 +18,27 @@ def get_query_results(query):
 	
 def display_query_results(query_results):
     for i in range(len(query_results)):
-        title = query_results[i][0]
-        views = query_results[i][1]
-        print("    %s == %d" % (title, views))
+        title = query_results[i][1]
+        author_views = query_results[i][0]
+        print("    %s -- %d" % (title, author_views))
 
 
 def display_error_results(query_results):
     for i in range(len(query_results)):
         print(
-            "    %s == %s  == %s "%
+            "    %s -- %s  -- %s "%
             (query_results[i][0], query_results[i][1], "Errors"))
 
 print("*******************************************************")
-print("What r d most popular 3 articles of all time?")
+print("1. What are the most popular three articles of all time?")
 
-query_for_first_3_records = "SELECT title, count(title) \
-    AS views FROM articles, log \
+query_for_first_3_records = "SELECT count(title) \
+    AS author_views, title FROM articles, log \
     WHERE log.path LIKE concat('%',articles.slug) \
-    GROUP BY articles.title, articles.author ORDER BY views DESC  limit 3"
+    GROUP BY articles.title, articles.author ORDER BY author_views DESC  limit 3"
 
 
-get_first_3_articles = get_query_results(query_for_first_3_records)
+get_first_3_articles = pull_query_results_from_db(query_for_first_3_records)
 
 display_query_results(get_first_3_articles)
 
@@ -46,31 +46,28 @@ print("*******************************************************")
 print("\n")
 
 
-print("What are the most popular authors?")
+print("2. Who are the most popular article authors of all time?")
 
-query_for_most_popular_authors = "select authors.name, count(*) as views from articles inner \
+query_for_most_popular_authors = "select  count(*) as author_views , authors.name from articles inner \
 join authors on articles.author = authors.id inner join log \
     on log.path like concat('%', articles.slug, '%') \
-    group by authors.name order by views desc"
-
-
-get_famous_authors = get_query_results(query_for_most_popular_authors)
+    group by authors.name order by author_views desc"
+get_famous_authors = pull_query_results_from_db(query_for_most_popular_authors)
 get_famous_authors = display_query_results(get_famous_authors)
 
 print("*******************************************************")
 print("\n")
 
 
-print("Dates which r more than 1% lead to errors?")
-query_to_get_date_errors_morethan_1 = "select day, perc from ( \
-    select day, round((sum(requests)/(select count(*) from log where \
-    substring(cast(log.time as text), 0, 11) = day) * 100), 2) as \
-    perc from (select substring(cast(log.time as text), 0, 11) as day, \
-    count(*) as requests from log where status like '%404%' group by day) \
-    as log_percentage group by day order by perc desc) as final_query \
-    where perc >= 1  "
+print("3. On which days did more than 1% of requests lead to errors?")
+query_to_get_date_errors_morethan_1 = "select error_date, percentage from ( \
+    select error_date, round((sum(requests)/(select count(*) from log where \
+    substring(cast(log.time as text), 0, 11) = error_date) * 100), 2) as \
+    percentage from (select substring(cast(log.time as text), 0, 11) as error_date, \
+    count(*) as requests from log where status like '%404%' group by error_date) \
+    as log_percentage group by error_date order by percentage desc) as last_query \
+    where percentage >= 1  "
 
-get_famous_authors = get_query_results(query_to_get_date_errors_morethan_1)
-
+get_famous_authors = pull_query_results_from_db(query_to_get_date_errors_morethan_1)
 get_famous_authors = display_error_results(get_famous_authors)
 print("*******************************************************")
